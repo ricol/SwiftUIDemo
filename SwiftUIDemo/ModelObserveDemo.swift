2e1b3bc9b5f2d2b27231982ba89bfff3bd3c009b
Ricol Wang <ricol.wang@ef.com>
2025-02-10 16:45:30 +0800

    update

---
+++ SwiftUIDemo/ModelObserveDemo.swift
@@ -0,0 +1,87 @@
+//
+//  DemoView.swift
+//  SwiftUIDemo
+//
+//  Created by ricolwang on 2025/2/10.
+//
+
+import SwiftUI
+
+struct ModelObserveDemo {
+    class SubViewModel: ObservableObject, Identifiable {
+        let id = UUID()
+        @Published var flag: Bool = false
+        let title: String = "hello"
+    }
+    
+    class ViewModel: ObservableObject {
+        @Published var data: [SubViewModel] = [SubViewModel(),
+                                               SubViewModel(),
+                                               SubViewModel(),
+                                               SubViewModel()
+        ]
+    }
+    
+    struct DemoView: View {
+        @StateObject var vm: ViewModel = ViewModel()
+        
+        var body: some View {
+            VStack {
+                ForEach(vm.data) { m in
+                    SubView(m: m)
+                }
+            }
+        }
+    }
+    
+    struct SubView: View {
+        @ObservedObject var m: SubViewModel
+        var body: some View {
+            Button {
+                m.flag.toggle()
+            } label: {
+                Text(m.title).background(m.flag ? .red : .clear)
+            }
+        }
+    }
+}
+
+struct ModelObserveWrongWayDemo {
+    class SubViewModel: ObservableObject, Identifiable {
+        let id = UUID()
+        @Published var flag: Bool = false
+        let title: String = "hello"
+    }
+    
+    class ViewModel: ObservableObject {
+        @Published var data: [SubViewModel] = [SubViewModel(),
+                                               SubViewModel(),
+                                               SubViewModel(),
+                                               SubViewModel()
+        ]
+    }
+    
+    struct DemoView: View {
+        @StateObject var vm: ViewModel = ViewModel()
+        
+        var body: some View {
+            VStack {
+                ForEach(vm.data) { m in
+                    Button {
+                        m.flag.toggle()
+                    } label: {
+                        Text(m.title).background(m.flag ? .red : .clear)
+                    }
+                }
+            }
+        }
+    }
+}
+
+#Preview {
+    ModelObserveDemo.DemoView(vm: ModelObserveDemo.ViewModel())
+}
+
+#Preview {
+    ModelObserveWrongWayDemo.DemoView(vm: ModelObserveWrongWayDemo.ViewModel())
+}
